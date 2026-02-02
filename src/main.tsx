f2db301c8809f570784ca79ef3f2f8d9bfa3b920
Igor Kaliel <igorkaliel@gmail.com>
2026-02-02 12:11:16 -0300

    fix: include in main component the SpeedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./app/styles/global.css"
 import App from "./app/App.tsx"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>,
 )
